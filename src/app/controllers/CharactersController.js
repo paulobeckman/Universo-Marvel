d89ff6f080fa4deae8ec6583fb4ea016f3f56c78
paulobeckman <beckmam.paulo@hotmail.com>
2021-07-31 00:20:14 -0300

    feat: get comics character

--- src/app/controllers/CharactersController.js
+++ src/app/controllers/CharactersController.js
@@ -1,4 +1,5 @@
 require('dotenv/config');
+const { response } = require('express');
 const api = require('../../services/api')
 
 module.exports = {
@@ -16,11 +17,20 @@ module.exports = {
         const { APP_API_KEY, APP_HASH } = process.env
         const { id } = req.params
 
-        const character = await api.get(`characters/${id}?limit=2&ts=1616200616&apikey=${APP_API_KEY}&hash=${APP_HASH}`)
-            .then(response => {
-                return response.data.data.results[0]
-            });
+        const results = await api.get(`characters/${id}?limit=2&ts=1616200616&apikey=${APP_API_KEY}&hash=${APP_HASH}`)
+        const {results: character } = results.data.data
+        
+        const comics = character[0].comics.items.map( async comic => {
+            const comicId = comic.resourceURI.split('/').reverse()[0]
+            const newComic = await api.get(`comics/${comicId}?limit=50&ts=1616200616&apikey=${APP_API_KEY}&hash=${APP_HASH}`)
+            return newComic.data.data.results[0]
+        })
 
-        return res.render('Characters/show', {character})
+        const result = await Promise.all(comics)
+
+        // console.log(JSON.stringify(character[0], null, 2))
+        console.log(JSON.stringify(result[0].title, null, 2))
+
+        return res.render('Characters/show', {character:character[0], comics: result})
     }
 }
