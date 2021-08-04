08cb287e0e23c2628d6839716d3a443ebcb53bdd
paulobeckman <beckmam.paulo@hotmail.com>
2021-08-03 23:58:36 -0300

    feat: finished create page series details

--- src/app/controllers/SeriesController.js
+++ src/app/controllers/SeriesController.js
@@ -19,9 +19,21 @@ module.exports = {
         const response = await api.get(`series/${id}?limit=2&ts=1616200616&apikey=${APP_API_KEY}&hash=${APP_HASH}`)
         const serie = response.data.data.results[0]
 
-        console.log(JSON.stringify(serie, null, 2))
+        const creators = serie.creators.items.map( async creator => {
+            const creatorId = creator.resourceURI.split('/').reverse()[0]
+            const newCreator = await api.get(`creators/${creatorId}?limit=50&ts=1616200616&apikey=${APP_API_KEY}&hash=${APP_HASH}`)
+            return newCreator.data.data.results[0]
+        })
+        const resultCreators = await Promise.all(creators)
 
-        return res.render('Series/show', {serie})
+        const comics = serie.comics.items.map( async comic => {
+            const comicId = comic.resourceURI.split('/').reverse()[0]
+            const newComic = await api.get(`comics/${comicId}?limit=50&ts=1616200616&apikey=${APP_API_KEY}&hash=${APP_HASH}`)
+            return newComic.data.data.results[0]
+        })
+        const resultComics = await Promise.all(comics)
+
+        return res.render('Series/show', {serie, creators: resultCreators, comics: resultComics})
     }
 
 }
